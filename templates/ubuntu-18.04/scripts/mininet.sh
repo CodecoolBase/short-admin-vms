0bd321eb22188977e735a09c2f6d198c3c086b03
Kohányi Róbert <kohanyi.robert@gmail.com>
2020-10-01 21:40:40 +0200

    Install pip and ipaddress module for Mininet

--- templates/ubuntu-18.04/scripts/mininet.sh
+++ templates/ubuntu-18.04/scripts/mininet.sh
@@ -4,3 +4,6 @@ systemctl stop openvswitch-testcontroller
 systemctl disable openvswitch-testcontroller
 systemctl enable openvswitch-switch
 systemctl start openvswitch-switch
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install ipaddress
